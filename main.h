ac08f02cbb86d678b43faea01f13f87b1000d25e
wesam-c <wesameldeeb15@gmail.com>
2023-11-12 23:56:14 -0800

    add

--- main.h
+++ main.h
@@ -16,5 +16,6 @@ int print_int(va_list args);
 int print_dec(va_list args);
 int print_bin(va_list val);
 int print_unsigned(va_list args);
-
+int print_oct(va_list val);
+int print_hex(va_list val);
 #endif
